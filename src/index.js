501de8bfe4735cbd590ce5e6ba5f1333030f7b84
Matteo Landi <matteo@matteolandi.net>
2017-02-13 00:56:48 +0100

    Replace current url if spreadsheet ID is missing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import './index.css';
 
-const spreadsheetId = window.location.hash.substring(1) || '1akhbqFlElr0iKnKmK0ZTQeNTBgvNgZVghEpDR9_MDvY';
+let spreadsheetId = window.location.hash.substring(1);
+if (!spreadsheetId) {
+  spreadsheetId = '1akhbqFlElr0iKnKmK0ZTQeNTBgvNgZVghEpDR9_MDvY';
+  window.location.replace(`./#${spreadsheetId}`);
+}
+
 ReactDOM.render(
   <App
     refreshInterval={ 60 * 60 * 1000 }
